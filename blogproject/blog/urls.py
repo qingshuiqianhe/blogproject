fef43a30341ab2315be2c01986aeec28017322b1
qingshuiqianhe <qingshuiqian12@gmail.com>
2017-10-02 16:23:51 +0800

    blogproject

---
+++ blogproject/blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+app_name = 'blog'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
+    url(r'^search/$', views.search, name='search'),
+]
---
+++ blogproject/love/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'love/index.html', )
+
+
+
+
+
